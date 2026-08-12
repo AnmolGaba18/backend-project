b24a4398192f1b0614190da227efea54d8a4aef0
AnmolGaba18 <gabaanmol45@gmail.com>
2026-08-12 21:20:07 +0530

    add register controller and bug fixes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -2,7 +2,7 @@ import mongoose, { Schema } from "mongoose"
 import jwt from "jsonwebtoken"
 import bcrypt from "bcrypt"
 
-const userSchema = new (
+const userSchema = new Schema(
     {
         username: {
             type: String,
@@ -58,7 +58,7 @@ const userSchema = new (
 userSchema.pre("save", async function (next) {
     if (!this.isModified("password")) return next();
 
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
